920b28e58484c9a2ed8cefa08d7749e8c24511a2
salieribaba <kanuniirfan@gmail.com>
2023-01-07 07:36:35 +0300

    com_002

--- controllers/product.js
+++ controllers/product.js
@@ -1,138 +1,147 @@
-import Product from '../models/product.js';
-import Category from '../models/category.js';
-import fs from 'fs';
-import slugify from 'slugify';
-
+import Product from "../models/product.js";
+import Category from "../models/category.js";
+import fs from "fs";
+import slugify from "slugify";
 
 //ürünleri category ile birlikte listele
-export const getProducts = async (req, res) => {
-    try {
-        const products = await Product.find({}).populate('category').select('-photo').limit(10).sort({ createdAt: -1 });
-        res.json(products);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send('Server Error');
-    }
-};
-
-//ürünü category ile birlikte listele
-export const getProductById = async (req, res) => {
-    try {
-        const product = await Product.findById(req.params.id).populate('category').select('-photo');
-        res.json(product);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send('Server Error');
-    }
-};
-
-//ürünü category ile birlikte oluştur
 export const create = async (req, res) => {
-    try {
-       const { name, description, price, category,  quantity, shipping } = req.fields;
-        const { photo } = req.files;
-        
-        switch (true) { 
-            case !name.trim():
-                return res.status(400).json({ error: 'Ürün adı boş bırakılamaz' });
-            case !price.trim():
-                return res.status(400).json({ error: 'Ürün fiyatı boş bırakılamaz' });
-            case !category.trim():
-                return res.status(400).json({ error: 'Ürün kategorisi boş bırakılamaz' });
-            case !quantity.trim():
-                return res.status(400).json({ error: 'Ürün adedi boş bırakılamaz' });
-            case !shipping.trim():
-                return res.status(400).json({ error: 'Ürün kargo bilgisi boş bırakılamaz' });
-            case photo && photo.size > 1000000:
-                return res.status(400).json({ error: 'Resim boyutu 1 Mb. tan büyük olamaz!' });
-            default:
-                break;
-        }
+  try {
+    // console.log(req.fields);
+    // console.log(req.files);
+    const { name, description, price, category, quantity, shipping } =
+      req.fields;
+    const { photo } = req.files;
 
-       const product = new Product({ ...req.fields, slug: slugify(name) });
-       if (photo) {
+    // validation
+    switch (true) {
+      case !name.trim():
+        res.json({ error: "Name is required" });
+      case !description.trim():
+        res.json({ error: "Description is required" });
+      case !price.trim():
+        res.json({ error: "Price is required" });
+      case !category.trim():
+        res.json({ error: "Category is required" });
+      case !quantity.trim():
+        res.json({ error: "Quantity is required" });
+      case !shipping.trim():
+        res.json({ error: "Shipping is required" });
+      case photo && photo.size > 1000000:
+        res.json({ error: "Image should be less than 1mb in size" });
+    }
+
+    // create product
+    const product = new Product({ ...req.fields, slug: slugify(name) });
+
+    if (photo) {
       product.photo.data = fs.readFileSync(photo.path);
       product.photo.contentType = photo.type;
     }
-      await product.save();
-        res.json(product);
 
-    
-   } catch (error) {
-        console.error(error.message);
-        res.status(400).send('Hatalı istek');
-       res.status(500).send('Server Error');
-   }
+    await product.save();
+    res.json(product);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json(err.message);
+  }
 };
 
-//ürün resmini göster
+export const list = async (req, res) => {
+  try {
+    const products = await Product.find({})
+      .populate("category")
+      .select("-photo")
+      .limit(12)
+      .sort({ createdAt: -1 });
+
+    res.json(products);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const read = async (req, res) => {
+  try {
+    const product = await Product.findOne({ slug: req.params.slug })
+      .select("-photo")
+      .populate("category");
+
+    res.json(product);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 export const photo = async (req, res) => {
-    try {
-        const product = await Product.findById(req.params.productId).select('photo');
-        if (product.photo.data) {
-            res.set('Content-Type', product.photo.contentType);
-            return res.send(product.photo.data);
-        }
-    } catch (error) {
-        console.error(error.message);
-        res.status(400).send('Fotoğraf bulunamadı.');
-        res.status(500).send('Server Error');
+  try {
+    const product = await Product.findById(req.params.productId).select(
+      "photo"
+    );
+    if (product.photo.data) {
+      res.set("Content-Type", product.photo.contentType);
+      return res.send(product.photo.data);
     }
+  } catch (err) {
+    console.log(err);
+  }
 };
- 
-//ürünü sil
+
 export const remove = async (req, res) => {
-    try {
-        const product = await Product.findByIdAndDelete(req.params.productId);
-        res.json({ message: 'Ürün silindi' });
-    } catch (error) {
-        console.error(error.message);
-        res.status(400).send('Hatalı istek');
-        res.status(500).send('Server Error');
-    }
+  try {
+    const product = await Product.findByIdAndDelete(
+      req.params.productId
+    ).select("-photo");
+    res.json(product);
+  } catch (err) {
+    console.log(err);
+  }
 };
- 
-//ürünü güncelle
+
 export const update = async (req, res) => {
-    try {
-        const { name, description, price, category,  quantity, shipping } = req.fields;
-        const { photo } = req.files;
-        
-        switch (true) { 
-            case !name.trim():
-                return res.status(400).json({ error: 'Ürün adı boş bırakılamaz' });
-            case !price.trim():
-                return res.status(400).json({ error: 'Ürün fiyatı boş bırakılamaz' });
-            case !category.trim():
-                return res.status(400).json({ error: 'Ürün kategorisi boş bırakılamaz' });
-            case !quantity.trim():
-                return res.status(400).json({ error: 'Ürün adedi boş bırakılamaz' });
-            case !shipping.trim():
-                return res.status(400).json({ error: 'Ürün kargo bilgisi boş bırakılamaz' });   
-            case photo && photo.size > 1000000:
-                return res.status(400).json({ error: 'Resim boyutu 1 Mb. tan büyük olamaz!' });
-            default:
-                break;
-        }
+  try {
+    // console.log(req.fields);
+    // console.log(req.files);
+    const { name, description, price, category, quantity, shipping } =
+      req.fields;
+    const { photo } = req.files;
 
-        let product = await Product.findByIdAndUpdate(req.params.productId);
-        if (!product) return res.status(400).json({ error: 'Ürün bulunamadı' });
-        product
-            .set({ ...req.fields, slug: slugify(name) })
-            .set({ updatedAt: Date.now() });
-        if (photo) {
-            product.photo.data = fs.readFileSync(photo.path);
-            product.photo.contentType = photo.type;
-        }
-        await product.save();
-        res.json(product);
+    // validation
+    switch (true) {
+      case !name.trim():
+        res.json({ error: "Name is required" });
+      case !description.trim():
+        res.json({ error: "Description is required" });
+      case !price.trim():
+        res.json({ error: "Price is required" });
+      case !category.trim():
+        res.json({ error: "Category is required" });
+      case !quantity.trim():
+        res.json({ error: "Quantity is required" });
+      case !shipping.trim():
+        res.json({ error: "Shipping is required" });
+      case photo && photo.size > 1000000:
+        res.json({ error: "Image should be less than 1mb in size" });
+    }
 
+    // update product
+    const product = await Product.findByIdAndUpdate(
+      req.params.productId,
+      {
+        ...req.fields,
+        slug: slugify(name),
+      },
+      { new: true }
+    );
+
+    if (photo) {
+      product.photo.data = fs.readFileSync(photo.path);
+      product.photo.contentType = photo.type;
     }
-    catch (error) {
-        console.error(error.message);
-        res.status(400).send('Hatalı istek');
-        res.status(500).send('Server Error');
-    }
+
+    await product.save();
+    res.json(product);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json(err.message);
+  }
 };
-
-
--- routes/product.js
+++ routes/product.js
@@ -1,20 +1,21 @@
-import express from 'express';
-import { requireSignin, isAdmin } from '../middlewares/auth.js';
-import { getProducts, getProductById, create, photo, remove, update } from '../controllers/product.js';
-import formidable from 'express-formidable';
+import express from "express";
+import { requireSignin, isAdmin } from "../middlewares/auth.js";
+import {
+  list,
+  read,
+  create,
+  photo,
+  remove,
+  update,
+} from "../controllers/product.js";
+import formidable from "express-formidable";
 const router = express.Router();
 
-
-router.get('/products', getProducts);
-router.get('/product/:id', getProductById);
-router.post('/product', requireSignin, isAdmin, formidable(), create);
-router.get('/product/photo/:productId', photo);
-router.delete('/product/:productId', requireSignin, isAdmin, remove);
-router.put('/product/:productId', requireSignin, isAdmin, formidable(), update);
-
-
-
-
-
+router.post("/product", requireSignin, isAdmin, formidable(), create);
+router.get("/products", list);
+router.get("/product/:slug", read);
+router.get("/product/photo/:productId", photo);
+router.delete("/product/:productId", requireSignin, isAdmin, remove);
+router.put("/product/:productId", requireSignin, isAdmin, formidable(), update);
 
 export default router;
